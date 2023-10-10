0d8e33acf95cb5084a9be4f929054b0a96d4d710
Natan Zabłocki <ZabloEmail@gmail.com>
2023-10-10 19:24:13 +0200

    Dodano do projektu FluentValidation

---
+++ pracaInż/Validators/FactoryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using pracaInż.Models.Entities.CompanyStructure;
+
+namespace pracaInż.Validators
+{
+    public class FactoryValidator : AbstractValidator<Factory>
+    {
+        public FactoryValidator()
+        {
+            RuleFor(Factory => Factory.City)
+                .NotEmpty()
+                .WithMessage("Pole jest wymagane!");
+
+        }
+    }
+}
