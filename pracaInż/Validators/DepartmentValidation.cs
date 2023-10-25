0ae8cd30b4ef29b92393b303dc0bcfa208983c46
Natan Zabłocki <ZabloEmail@gmail.com>
2023-10-25 14:17:40 +0200

    Walidacja Obiektu "Dział"

--- pracaInż/Validators/DepartmentValidation.cs
+++ pracaInż/Validators/DepartmentValidation.cs
@@ -8,7 +8,29 @@ namespace pracaInż.Validators
         public DepartmentValidation() 
         {
             RuleFor(dep => dep.Name)
-                .NotEmpty().WithMessage("Tole nazwa nie może być pusta!");
+                .NotEmpty().WithMessage("Pole nazwa nie może być pusta!")
+                .Must(BeValidName).WithMessage("Pole nazwa może składać się tylko z liter!")
+                .MinimumLength(5).WithMessage("Nazwa zbyt krótka!");
+
+
+            RuleFor(dep => dep.ShortName)
+                .NotEmpty().WithMessage("Pole skrót nie może być pusta!")
+                .Must(BeValidName).WithMessage("Pole skrót może składać się tylko z liter!")
+                .MinimumLength(1).WithMessage("Pole skrót musi zawierać co najmniej 2 znaki!")
+                .MaximumLength(5).WithMessage("Pole skrót musi być krótsze niż 5 znaków!");
+
+            RuleFor(dep => dep.InvoiceCode)
+                .NotEmpty().WithMessage("Kod jednostki organizacyjnej nie może być pusty!")
+                .MaximumLength(20).WithMessage("Kod jednostki organizacyjnej jest zbyt długi!")
+                .MinimumLength(5).WithMessage("Kod jednostki organizacyjnej ejst zbyt krótki!");
+
+            RuleFor(dep => dep.FactoryId)
+                .NotEmpty().WithMessage("Nie przypisany działu do żadnej fabryki");
+        }
+
+        public bool BeValidName(string name)
+        {
+            return name.All(char.IsLetter);
         }
     }
 }
