235e371b222a42abbdd837a3f3e1488dbead7136
Zablo100 <ZabloEmail@gmail.com>
2023-11-09 15:25:50 +0100

    [Backend] Poprawiono walidacje dla tworzenia obiektu Dział

--- pracaInż/Validators/DepartmentValidation.cs
+++ pracaInż/Validators/DepartmentValidation.cs
@@ -30,7 +30,7 @@ namespace pracaInż.Validators
 
         public bool BeValidName(string name)
         {
-            return name.All(char.IsLetter);
+            return name.All(c => char.IsLetter(c) || char.IsWhiteSpace(c));
         }
     }
 
