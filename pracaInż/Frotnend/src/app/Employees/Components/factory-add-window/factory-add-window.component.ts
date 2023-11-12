f1b1a063d327d73edf4a5122e60c58a900881a93
Zablo100 <ZabloEmail@gmail.com>
2023-11-12 14:28:23 +0100

    [Frontend] Dodano okno tworzenie nowej fabryki

--- pracaInż/Frotnend/src/app/Employees/Components/factory-add-window/factory-add-window.component.ts
+++ pracaInż/Frotnend/src/app/Employees/Components/factory-add-window/factory-add-window.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Employee } from 'src/app/Models/Employee';
+import { EmployeeService } from '../../employee.service';
+import { ToastrService } from 'ngx-toastr';
+import { getErrorMessage } from 'src/app/Core/appip';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-factory-add-window',
@@ -6,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./factory-add-window.component.css']
 })
 export class FactoryAddWindowComponent implements OnInit {
-
-  constructor() { }
+  AddForm: FormGroup;
+  constructor(public dialogRef: MatDialogRef<FactoryAddWindowComponent>,private service: EmployeeService, private notification: ToastrService) { }
 
   ngOnInit(): void {
+    this.createFormGroup();
   }
+
+  createFormGroup(){
+    this.AddForm = new FormGroup({
+      city: new FormControl(""),
+      street: new FormControl(""),
+      streetNumber: new FormControl(""),
+      postalCode: new FormControl("")
+    })
+  }
+
+  submit(){
+    this.service.addNewFactory(this.AddForm.value).subscribe(() => {
+      this.notification.success("Pomyślnie dodano fabryke")
+      this.dialogRef.close()
+    }, (err) => {
+      this.notification.error(getErrorMessage(err))
+    })
+  
+  }
+
+
 
 }
