4f2a2b887584bcb4b5e54d2da93459b0e3b69e6f
Zablo100 <ZabloEmail@gmail.com>
2023-11-12 14:29:38 +0100

    [Frotnend] Dodano funkcje do automatycznego sprawdzania rodzaju błędu i wyświetlania odpowiedniej wiadomości

--- pracaInż/Frotnend/src/app/Core/appip.ts
+++ pracaInż/Frotnend/src/app/Core/appip.ts
@@ -1,6 +1,15 @@
 const host = "224";
 const local = "localhost:7096";
 const myip = `192.168.1.${host}:4040`;
+
 export const app = {
     ip: `${local}`
 };
+
+export function getErrorMessage(err: any){
+    if (err.error.description != undefined){
+        return err.error.description
+    }
+
+    return "Błąd serwera!"
+}
