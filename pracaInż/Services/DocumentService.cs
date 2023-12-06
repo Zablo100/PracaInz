363781bfbde8b748345f199d5dd6b2a513ea35cf
Zablo100 <ZabloEmail@gmail.com>
2023-12-06 20:07:06 +0100

    [Backend] Dodano metody serwisu dla dodawania nowych dokumentów

--- pracaInż/Services/DocumentService.cs
+++ pracaInż/Services/DocumentService.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using pracaInż.Data;
 using pracaInż.Models.Entities.Documents;
+using System.Text;
 
 namespace pracaInż.Services
 {
@@ -9,6 +10,8 @@ namespace pracaInż.Services
     {
         Task<ErrorOr<DocumentModel>> GetManualDocumentById(int id);
         Task<ErrorOr<List<DocumentModel>>> GetAllDocuments();
+        Task<ErrorOr<Created>> UploadNewFile(DocumentModel document, IFormFile file);
+        Task<ErrorOr<Updated>> UpdateDocument(IFormFile file);
     }
     public class DocumentService : IDocumentService
     {
@@ -45,6 +48,77 @@ namespace pracaInż.Services
 
             result = doc;
             return result;
+        }
+
+        public async Task<ErrorOr<Updated>> UpdateDocument(int id, IFormFile file)
+        {
+            ErrorOr<Updated> result;
+            var doc = await _context.Documents.FindAsync(id);
+            if(doc == null)
+            {
+                result = Error.NotFound(description: "Wystąpił błąd podczas ładowania pliku");
+                return result;
+            }
+
+
+            var filePath = Path.Combine(Environment.CurrentDirectory, "Files", file.FileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                try
+                {
+                    await file.CopyToAsync(stream);
+                }
+                catch (IOException e)
+                {
+                    result = Error.Failure(description: "Wystąpił nieoczekiwany błąd poczas zapisywania pliku!");
+                    //TODO: Log exception
+                }
+            }
+
+            if (doc.PathToFile != filePath)
+            {
+                doc.PathToFile = filePath;
+            }
+
+            result = Result.Updated;
+            return result;
+        }
+
+        public async Task<ErrorOr<Created>> UploadNewFile(DocumentModel document, IFormFile file)
+        {
+            ErrorOr<Created> result;
+            if(file == null || file.Length == 0)
+            {
+                result = Error.NotFound();
+                return result;
+            }
+
+            //TODO: walidacja
+            var filePath = Path.Combine(Environment.CurrentDirectory, "Files", file.FileName);
+
+            if (File.Exists(filePath))
+            {
+                result = Error.Conflict(description: "Plik już istnieje!");
+                return result;
+            }
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                try
+                {
+                    await file.CopyToAsync(stream);
+                }
+                catch(IOException e)
+                {
+                    result = Error.Failure(description: "Wystąpił nieoczekiwany błąd poczas zapisywania pliku!");
+                    //TODO: Log exception
+                }
+            }
+
+            result = Result.Created;
+            return result;
+
         }
     }
 }
